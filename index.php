e1c42f5c087118c4c0a29f8e27f7683ad8927b3d
Andrew <agalavan@student.unit.ua>
2018-08-23 15:17:15 +0300

    set up router, add pages templates

--- index.php
+++ index.php
@@ -1,18 +1,36 @@
 <?php 
 
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
 require __DIR__ . '/vendor/autoload.php';
 date_default_timezone_set('Europe/Kiev');
 
-use Monolog\Logger;
-use Monolog\Handler\StreamHandler;
+// use Monolog\Logger;
+// use Monolog\Handler\StreamHandler;
 
 // $log = new Logger('name');
 // $log->pushHandler(new StreamHandler('app.log', Logger::WARNING));
 // $log->addWarning('Foo');
 
-$app = new \Slim\Slim();
+$app = new \Slim\Slim(array(
+    'view' => new \Slim\Views\Twig()
+));
 
-$app->get('/hello/:name', function ($name) {
-    echo "Hello, $name";
+$view = $app->view();
+$view->parserOptions = array(
+    'debug' => true
+);
+
+$app->get('/', function () use($app) {
+    $app->render('about.twig');
 });
+$view->parserExtensions = array(
+    new \Slim\Views\TwigExtension(),
+);
+
+$app->get('/contact', function () use($app) {
+    $app->render('contact.twig');
+});
+
 $app->run();
