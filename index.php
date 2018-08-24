a60bcc4a0ff7058025b826c0a7d98c8b3d57fc13
Andrew <agalavan@student.unit.ua>
2018-08-24 15:47:17 +0300

    fix bugs and now all works fine: 2 pages, contact form

--- index.php
+++ index.php
@@ -1,20 +1,13 @@
-<?php 
-
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-
+<?php
 require 'vendor/autoload.php';
-date_default_timezone_set('Europe/Kiev');
+date_default_timezone_set('America/New_York');
 
-// use Monolog\Logger;
-// use Monolog\Handler\StreamHandler;
+//$log = new Monolog\Logger('name');
+//$log->pushHandler(new Monolog\Handler\StreamHandler('app.txt', Monolog\Logger::WARNING));
+//$log->addWarning('Oh Noes.');
 
-// $log = new Logger('name');
-// $log->pushHandler(new StreamHandler('app.log', Logger::WARNING));
-// $log->addWarning('Foo');
-
-$app = new \Slim\Slim(array(
-    'view' => new \Slim\Views\Twig()
+$app = new \Slim\Slim( array(
+  'view' => new \Slim\Views\Twig()
 ));
 
 $view = $app->view();
@@ -25,12 +18,52 @@ $view->parserExtensions = array(
     new \Slim\Views\TwigExtension(),
 );
 
-$app->get('/', function() use($app) {
-    $app->render('about.twig');
-});
+$app->get('/', function() use($app){
+  $app->render('about.twig');
+})->name('home');
 
-$app->get('/contact', function() use($app) {
-    $app->render('contact.twig');
+$app->get('/contact', function() use($app){
+  $app->render('contact.twig');
+})->name('contact');
+
+$app->post('/contact', function() use($app){
+  $name = $app->request->post('name');
+  $email = $app->request->post('email');
+  $msg = $app->request->post('msg');
+  
+  if(!empty($name) && !empty($email) && !empty($msg)) {
+    $cleanName = filter_var($name, FILTER_SANITIZE_STRING);
+    $cleanEmail = filter_var($email, FILTER_SANITIZE_EMAIL);
+    $cleanMsg = filter_var($msg, FILTER_SANITIZE_STRING);
+  } else {
+    //message the user that there was a problem
+    $app->redirect('/contact');
+  }
+  
+  $transport = Swift_SendmailTransport::newInstance('/usr/sbin/sendmail -bs');
+  $mailer = \Swift_Mailer::newInstance($transport);
+  
+  $message = \Swift_Message::newInstance();
+  $message->setSubject('Email From Our Website');
+  $message->setFrom(array(
+     $cleanEmail => $cleanName
+  ));
+  $message->setTo(array('treehouse@localhost'));
+  $message->setBody($cleanMsg);
+  
+  $result = $mailer->send($message);
+
+  if($result > 0) {
+    // send a message that says thank you.
+    $app->redirect('/');
+   
+  } else {
+    // send a message to the user that the message failed to send
+    // log that there was an error
+    $app->redirect('/contact');
+  }
+  
+  
 });
 
 $app->run();
